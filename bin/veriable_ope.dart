a0ee5f21d1e5d0f90815f965c176c4185b326135
tipnip-sumon <sumonmti498@gmail.com>
2025-04-27 16:57:27 +0600

    First commit

--- bin/veriable_ope.dart
+++ bin/veriable_ope.dart
@@ -1,6 +1,122 @@
 import 'dart:io';
-
 void main(){
+  // try{
+    // List<String> student = ["Shahariar","Samiul","Rafsan","Raisa"];
+    // List<int> amount = [500,1000,2500,750];
+    // print(student.length);
+    // for(int i = 0; i < student.length; i++){
+    //   print('Name $i is ${student[i]} \n ${amount[i]}');
+    // }
+  //   List<int> number = [1,2,3,4,5,6,7,8,9,10];
+  //   for(int i=0; i<=10;i+3){
+  //     print(number[i]);
+  //   }
+  // }catch(e){
+  //   print("Error $e");
+  // }
+  // for(int i = 0; i<=10; i++){
+  //   print("$i times");
+  // }
+  /*try{
+    print("Enter your age");
+    String ? input = stdin.readLineSync();
+    int age = int.parse(input!);
+    age > 18  ? print('Success') : print('No');
+  }catch(e){
+    print("Error $e");
+  }*/
+  // String ? name;
+  // print(name);
+  // late String address;
+  // address = 'Harati';
+  // print(address);
+  // String day = 'Sun';
+  // print("Enter Month");
+  // int ? month = int.parse(stdin.readLineSync()!);
+  // print(month);
+  // String day = 'Fri';
+  // print('Enter Your Name');
+  // String ? name = stdin.readLineSync();
+  // print(name);
+  // switch(day){
+  //   case 'Sun':
+  //     print('Relux');
+  //     break;
+  //   case 'Mon':
+  //     print('School open');
+  //     break;
+  //   default:
+  //     print('No comment');
+  // }
+  // var amount = 1000;
+  // String ? name;
+  // name = 'Sumon';
+  // print(name.toUpperCase());
+  // amount >= 1100 ? print("correct") : amount < 500 ?   print("Incorrect") : print("hete jabo");
+//   Set <String> colors = {'Red','Yellow','Pink'};
+//
+// colors.add('Black');
+// colors.addAll({"magenta",'Dark'});
+//   print(colors);
+  // var marks = 66;
+  // switch(marks) {
+  //   case  80:
+  //     print("Grade is A+");
+  //   break;
+  //
+  //   case 50:
+  //     print("Grade is B");
+  //     break;
+  //   default:
+  //     print("No results found");
+  //   }
+  // var student = new Map();
+  // student['name']='Samiul Islam Rafi';
+  // student['age']= 11;
+  // student['city']='Dhaka';
+  // student['country']='Bangladesh';
+  // print(student.length);
+
+
+
+
+
+
+//   var myCitySet = <String> {'Rangpur','Dhaka','Dinajpur'};
+//   myCitySet.addAll({'Cumilla','Chandpur'});
+//   myCitySet.clear();
+//   // print(myCitySet.elementAt(4));
+// print(myCitySet);
+
+
+
+
+
+
+
+
+
+
+
+  // var numbers = [1,2,3,4,5,6];
+  // numbers[1]=200;
+  // numbers.remove(numbers[0]);
+  // numbers.removeAt(3);
+  // numbers.removeLast();
+  // numbers.removeRange(0, 5);
+  // print(numbers);
+
+
+
+
+
+
+
+
+// var mapping = {'name':'Samiul Islam','age':11,'class':'six'};
+// print(mapping['name']);
+//   var list = ['Rangpur','Dhaka','Cumilla'];
+//   print(list.isNotEmpty);
   // int number1 = 10;
   // int number3 = 30;
   // String number5 = "26";
@@ -14,17 +130,17 @@ void main(){
   // int ? name = stdin.readByteSync();
   // print("Input your age: ");
   // int ? age = int.parse(stdin.readLineSync()!);
-  print("Enter Your Username");
-  String ? username = stdin.readLineSync();
-  print("Enter Your Password");
-  String ? password = stdin.readLineSync();
-  print("New number");
-  int ? number6 = int.parse(stdin.readLineSync()!);
-  if(number6 > 20 ){
-    print("No age");
-  } else if(username == 'rafi' && password == '123456') {
-    print('Login Successfully');
-  }
+  // print("Enter Your Username");
+  // String ? username = stdin.readLineSync();
+  // print("Enter Your Password");
+  // String ? password = stdin.readLineSync();
+  // print("New number");
+  // int ? number6 = int.parse(stdin.readLineSync()!);
+  // if(number6 > 20 ){
+  //   print("No age");
+  // } else if(username == 'rafi' && password == '123456') {
+  //   print('Login Successfully');
+  // }
   // print("My name is $name and my age is $age User name $username and $password");
 
 }
