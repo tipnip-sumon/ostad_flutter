7dedf65503720531a529e9836bd433fb9b391a57
tipnip-sumon <sumonmti498@gmail.com>
2025-04-28 16:37:37 +0600

    Optional parameter

--- bin/function.dart
+++ bin/function.dart
@@ -3,9 +3,10 @@ void main(){
   print(aria);
   print(calculationAria_2(5,6));
 }
-double calculationAria(double length, double width){
+//optional parameter
+double calculationAria(double length, double width,[String ? description]){
   double aria = length * width;
   return aria;
 }
 var calculationAria_2 = (double length,double width) => (length*width);
-//optional parameter
+
